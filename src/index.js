439dbfab9c6d7ad7fa6a83ab8b6c91febae1eeda
ubuntu <skyler.tabo@gmail.com>
2021-06-23 14:09:28 -0700

    created refactoring branch

--- src/index.js
+++ src/index.js
@@ -2,4 +2,20 @@ import { button } from "./button.js"
 import { status } from "./status.js"
 import { tasks } from "./tasks.js"
 
+
 button.makeButton()
+
+let activateButton = () => {
+    let newTaskButton = document.getElementById('newTaskButton')
+    newTaskButton.addEventListener('click', () => {
+        button.makeForm()
+        activateForm()
+    })
+}
+let activateForm = () => {
+    let buttonForm = document.getElementById('buttonForm')
+    buttonForm.addEventListener('submit', () => {
+        button.makeButton()
+        activateButton()
+    })
+}
